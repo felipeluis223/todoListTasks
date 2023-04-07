83252f0569d1e59e30742ecdab25ffc1af195db0
felipeluis223 <luis@up4tech.com.br>
2023-04-07 18:29:42 -0300

    feat: obtendo task e add em lista com id aleatório

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { useState } from 'react';
 import TodoTask from "./components/TodoTask/TodoTask";
-
 import './styles/styles.css'
 
+interface ITask{
+	id: number,
+	nameTask: string,
+}
+
 function App() {
+	const [ todoList, setTodoList ] = useState<ITask[]>([]);
 	const [ task, setTask ] = useState<string>("");
-    console.log(task);
+
+	// Add task with id and name:
+	const addTask = ()=>{
+		// Generate random id:
+		const idRandom = (num:number) => Math.floor(Math.random() * num);
+
+		const newTask = {
+			id: idRandom(100),
+			nameTask: task
+		}
+
+		// Add task in array:
+		setTodoList([...todoList, newTask]);
+
+	}
 
 	return (
 		<div className="App">
@@ -23,7 +42,7 @@ function App() {
 					onChange={(e)=>setTask(e.target.value)}
 				/>
 
-				<button type="submit" className="btn-header">Adicionar Task</button>
+				<button type="submit" className="btn-header" onClick={addTask}>Adicionar Task</button>
 			</header>
 			
 			<div className="line"></div>
